a68e08dd7e61f57858dfe9804eab82c66bf1baa3
BuildTools <unconfigured@null.spigotmc.org>
2022-05-29 20:13:35 +0200

    added val-ranked-v1 "Get leaderboard for the competitive queue"

---
+++ api/valRanked/leaderboard.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"encoding/json"
+	"val/api"
+)
+
+type LeaderBoardDto struct {
+	Shard        string      `json:"shard"`
+	ActId        string      `json:"actId"`
+	TotalPLayers int64       `json:"totalPlayers"`
+	Players      []PlayerDto `json:"players"`
+}
+
+type PlayerDto struct {
+	Puuid           string `json:"puuid"`
+	GameName        string `json:"gameName"`
+	TagLine         string `json:"tagLine"`
+	LeaderboardRank int64  `json:"leaderboardRank"`
+	RankedRating    int64  `json:"rankedRating"`
+	NumberOfWins    int64  `json:"numberOfWins"`
+}
+
+func GetLeaderBoard(apiKey, actId string) LeaderBoardDto {
+	client := api.NewClient(apiKey, "val/match/v1/matches/"+actId)
+
+	var leaderBoard = LeaderBoardDto{}
+
+	json.Unmarshal(client, &leaderBoard)
+
+	return leaderBoard
+}
